0338871f28049f01afbc1e27f6066bd8f835d9e1
TiredAJ <aletha.e.jones@gmail.com>
2024-09-28 18:47:26 +0200

    db stuff

---
+++ Src/Dataset/init.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE superheroes (
+    id SERIAL UNIQUE,
+    name TEXT NOT NULL,
+    description TEXT,
+    place TEXT
+);
+
+COPY superheroes
+FROM '/docker-entrypoint-initdb.d/superheroes.csv'
+DELIMITER ','
+CSV HEADER;
